d35c88ed3c341554d9bebfda144975f004623b8d
EDI SILFA <edisilfamirage@gmail.com>
2026-07-02 07:53:39 +0700

    added new file

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-            <title>Login ke Toko roti</title>
+            <title>My Florist</title>
             <script src="https://cdn.tailwindcss.com"></script>
     </head>
     <body class="bg-gray-100 flex items-center justify-center h-screen">
