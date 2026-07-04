cfa8b2646ec9b17f40ed08def26fbcbb4f3f0b81
EDI SILFA <edisilfamirage@gmail.com>
2026-07-04 09:54:13 +0700

    added new file

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -9,6 +9,6 @@ class TransactionController extends Controller
     //Tampilan form transaksi
     public function index(){
         $products = product::where([['stok','>',0]])->get();
-        return view('transactions.index', compact('product'));
+        return view('transactions.index', compact('products'));
     }
 }
